34a31d219cf6106ca693284d008380bcf3f49c68
ModMonster <modmonster2060@gmail.com>
2021-07-09 16:51:21 -0400

    Make timer command work.

--- mash/timer/timer.py
+++ mash/timer/timer.py
@@ -1,9 +1,14 @@
 # 1.0.0
 import os
-import winsound
 from time import sleep
+import threading
+import math
 
-cls = lambda: os.system('cls') # Clear screen
+# Clear screen
+if (os.name == "nt"):
+    cls = lambda: os.system('cls')
+else:
+    cls = lambda: os.system('clear')
 
 root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -11,6 +16,9 @@ file = open(root + "/shell/command.data", "r") # open file in read mode
 commands = file.read() # read commands
 args = commands.split(" ") # save args
 file.close()
+
+command = ""
+paused = False
 
 def ConvertTime(time):
     if (time[-1] == "d"):
@@ -29,24 +37,43 @@ def ConvertTime(time):
     
     return time
 
-def Time(time):
-    while (time > 0):
-        cls()
-        print("Press enter to pause and play timer. Type exit to exit timer. \n")
-        print(time)
+def Time():
+    global paused
+    global command
+    global dur
+    while (command != "exit"):
+        if (dur > 0):
+            if (not paused):
+                cls()
+                print("Press enter to pause and play timer. Type exit to exit timer. \n")
+                formattedDur = str(math.floor(dur / 3600) % 60) + ":" + str(math.floor(dur / 60) % 60).zfill(2) + ":" + str(dur % 60).replace(".0", "").zfill(2)
+                print(formattedDur)
 
-        time -= 1
-        sleep(1)
+                dur -= 1
+            sleep(1)
+        else:
+            print("\a") # beeeep!
+            command = "exit"
+            exit()
 
-    frequency = 2500  # Set Frequency To 2500 Hertz
-    duration = 1000  # Set Duration To 1000 ms == 1 second
-    winsound.Beep(frequency, duration)
+def WatchKeyboard():
+    global command
+    global paused
+    while (command != "exit"):
+        command = input()
+        if (command == ""):
+            paused = not paused
 
 if (len(args) >= 2):
     if (args[1] != ""):
         dur = ConvertTime(args[1])
         if (dur):
-            Time(dur)
+            thread1 = threading.Thread(target=Time)
+            thread1.start()
+
+            thread2 = threading.Thread(target=WatchKeyboard)
+            thread2.daemon = True
+            thread2.start()
         else:
             print("Invalid time format.")
     else:
